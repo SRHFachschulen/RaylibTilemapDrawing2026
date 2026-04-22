c05887c354401549b45eb01e07073c56a5e43afb
Oliver Pruchnicki <35111378+SquidSushi@users.noreply.github.com>
2026-04-22 09:36:26 +0200

    Added Raylib via NuGet and set up Window

--- MeinTilemapDrawer/Program.cs
+++ MeinTilemapDrawer/Program.cs
@@ -1,7 +1,17 @@
-﻿namespace MeinTilemapDrawer;
+﻿//using Raylib_cs;
+
+using Raylib_cs;
+
+namespace MeinTilemapDrawer;
 
 class Program{
     static void Main(string[] args){
         Console.WriteLine("Hello, World!");
+        Raylib.InitWindow(800,600,"Tilemaps! :D");
+        while (!Raylib.WindowShouldClose()){
+            Raylib.BeginDrawing();
+            Raylib.EndDrawing();
+        }
+        Raylib.CloseWindow();
     }
 }
