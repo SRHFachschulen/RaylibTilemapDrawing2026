c239b4ec0bfc79f2fd60e02387376c924661cb6e
Oliver Pruchnicki <35111378+SquidSushi@users.noreply.github.com>
2026-04-23 13:23:05 +0200

    Loading Screen (crashes!)

--- MeinTilemapDrawer/Program.cs
+++ MeinTilemapDrawer/Program.cs
@@ -8,8 +8,15 @@ class Program{
     static void Main(string[] args){
         Console.WriteLine("Hello, World!");
         Raylib.InitWindow(640,480,"Tilemaps! :D");
+        Raylib.BeginDrawing();
+        Raylib.ClearBackground(Color.White);
+        Raylib.DrawText("Loading...", 10, 10, 80, Color.Black);
+        Raylib.EndDrawing();
+        FileStream fs = new("ldtk/project/Level_0.ldtkl", FileMode.Open);
+        
         while (!Raylib.WindowShouldClose()){
             Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.White);
             Raylib.EndDrawing();
         }
         Raylib.CloseWindow();
